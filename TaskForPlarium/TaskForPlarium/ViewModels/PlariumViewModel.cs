e29cd0e181e29a2ca46b8767088c4a39c1fc4043
Misha <Misha@MISHA->
2016-06-04 17:16:33 +0300

    4 commit

--- TaskForPlarium/TaskForPlarium/ViewModels/PlariumViewModel.cs
+++ TaskForPlarium/TaskForPlarium/ViewModels/PlariumViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TaskForPlarium.Comands;
+using TaskForPlarium.Views;
 
 namespace TaskForPlarium.ViewModels
 {
     class PlariumViewModel:ViewModelIBase
     {
+
+        private DelegateCommand<string> digitButtonPressCommand;
+        public void ChooseButtonPress(string operation)
+        {
+            ChooseDirectory myWindow = new ChooseDirectory();
+            myWindow.Show();
+        }
+        public ICommand ChooseButtonPressCommand
+        {
+            get
+            {
+                if (digitButtonPressCommand == null)
+                {
+                    digitButtonPressCommand = new DelegateCommand<string>(
+                        ChooseButtonPress, (string button) => { return true; });
+                }
+                return digitButtonPressCommand;
+            }
+        }
     }
 }
---
+++ TaskForPlarium/TaskForPlarium/Views/ChooseDirectory.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskForPlarium.Views
+{
+    /// <summary>
+    /// Interaction logic for ChooseDirectory.xaml
+    /// </summary>
+    public partial class ChooseDirectory : Window
+    {
+        public ChooseDirectory()
+        {
+            InitializeComponent();
+        }
+    }
+}
